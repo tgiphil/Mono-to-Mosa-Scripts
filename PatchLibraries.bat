fe7104d81d7d62b8570efbc522f5ed35c27960f6
tgiphil <phil@thinkedge.com>
2010-04-19 21:31:10 -0700

    - Fixed XML files

--- PatchLibraries.bat
+++ PatchLibraries.bat
@@ -4,5 +4,5 @@ copy Patches\KeyValuePair.cs "%LIB%\mcs\class\corlib\System.Collections.Generic\
 copy %SOURCE%\mcs\build\common\Consts.cs.in "%LIB%\mcs\build\common\Consts.cs"
 
 copy Patches\Parser.cs "%LIB%\mcs\class\System.XML\System.Xml.XPath\"
-copy Patches\PatternParser.cs "%LIB%\mcs\class\System.XML\System.Xml.XPath\"
-copy Patches\PatternTokenizer.cs "%LIB%\mcs\class\System.XML\System.Xml.XPath\"
+copy Patches\PatternParser.cs "%LIB%\mcs\class\System.XML\Mono.Xml.Xsl\"
+copy Patches\PatternTokenizer.cs "%LIB%\mcs\class\System.XML\Mono.Xml.Xsl\"
